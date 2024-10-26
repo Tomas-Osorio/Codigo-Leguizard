6347c4c0d131d94e151ee35b4c4def4c5b424574
Tomas <tomyeosoriogmail.com>
2024-10-26 14:38:29 -0300

    Subidademotorriverh

--- Main/MotorDriver.h
+++ Main/MotorDriver.h
@@ -14,3 +14,22 @@ class MotorDriver {
 };
 
 #endif
+/*
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+#include <Arduino.h>
+
+class MotorDriver {
+  public:
+    MotorDriver(uint8_t pin);
+    bool init();
+    void drive(int value);
+  private:
+    uint8_t _pin;
+    int _currentSpeed; // Add this to track the current speed
+};
+
+#endif
+
+*/
