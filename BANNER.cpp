6a1805466ace77514ff214cd120a3f2263341a2c
magali mirabelli <tomyeosori@gmail.com>
2024-10-26 20:03:35 -0300

    Bannerimplementado

--- BANNER.cpp
+++ BANNER.cpp
@@ -1,14 +1,37 @@
-#include "BANNER.h"
+#include "BannerSensor.h"
 
-BANNER::BANNER() {
-
+// Constructor to initialize the sensor pin
+BannerSensor::BannerSensor(int pin) {
+  _pin = pin;
 }
 
-int BANNER::init() {
-    
+// Function to set up the sensor
+void BannerSensor::begin() {
+  pinMode(_pin, INPUT); // Set the pin as an input
 }
 
+// Function to check if an object is detected
+bool BannerSensor::isObjectDetected() {
+  return digitalRead(_pin) == HIGH; // Return true if the sensor detects an object
+}
+/*codigo de prueba:
+#include "BannerSensor.h"
 
-float BANNER::medir() {
+BannerSensor sensor(2); // Create a sensor object on pin 2
 
-} 
+void setup() {
+  Serial.begin(9600);
+  sensor.begin(); // Initialize the sensor
+}
+
+void loop() {
+  if (sensor.isObjectDetected()) {
+    Serial.println("Object detected");
+  } else {
+    Serial.println("No object detected");
+  }
+  
+  delay(500); // Wait for half a second before the next reading
+}
+
+*/
--- BANNER.h
+++ BANNER.h
@@ -1,13 +1,16 @@
-#ifndef BANNER_H
-#define BANNER_H
+#ifndef BANNERSENSOR_H
+#define BANNERSENSOR_H
 
 #include <Arduino.h>
 
-class  BANNER {
-public:
-    BANNER(); 
-    int init(); 
-    float medir();
+class BannerSensor {
+  public:
+    BannerSensor(int pin); // Constructor to initialize the sensor with a pin
+    void begin();          // Function to set up the sensor
+    bool isObjectDetected(); // Function to check if an object is detected
+
+  private:
+    int _pin; // The pin connected to the sensor
 };
 
 #endif
